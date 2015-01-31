be68fdf514fa00f59120721bc16e1f168552d6bb
Supasate Choochaisri <supasate.c@gmail.com>
2015-01-31 11:36:51 +0700

    Add debugger example.

---
+++ debugger/debugger.js
@@ -0,0 +1,8 @@
+// node debug debugger.js
+// type n for next
+// type repl to inspect value
+// - type var name to inspect var value
+// - ctrl-c to exit repl
+for (var i = 0; i < 10; i++) {
+   console.log(i);
+}
